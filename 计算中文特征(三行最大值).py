78f2f2eca0b7c5fc31fe476564ebbb94c8759e6e
Q_T_Pang <wwwpqting@163.com>
2018-06-24 11:01:27 +0800

    第二次修改

--- 计算中文特征(三行最大值).py
+++ 计算中文特征(三行最大值).py
@@ -18,47 +18,85 @@ def caculate_all(pathin):
         pic_path = os.path.join(pathin, picture)
         #  print pic_path
         im = cv.imread(pic_path, 0)
+        '''
+        三行取最大值
+        '''
         # 水平全穿越
-        HA.append(caculate_line(im[15]))
-        HA.append(caculate_line(im[31]))
-        HA.append(caculate_line(im[47]))
+        xlist1=im[14:17]
+        HA.append(caculate_three_lines(xlist1))
+        xlist2=im[30:33]
+        HA.append(caculate_three_lines(xlist2))
+        xlist3=im[46:49]
+        HA.append(caculate_three_lines(xlist3))
 
         # 竖直全穿越
-    	y1 = [y[15] for y in im]  # 获取列
-        y2 = [y[31] for y in im]
-        y3 = [y[47] for y in im]  
-        VA.append(caculate_line(y1))
-        VA.append(caculate_line(y2))
-        VA.append(caculate_line(y3))
+        y1 = [y[14] for y in im]  # 获取三列
+        y2 = [y[15] for y in im]  
+        y3 = [y[16] for y in im]  
+        ylist1=[y1,y2,y3]
+        VA.append(caculate_three_lines(ylist1))
+
+        y1 = [y[30] for y in im]  # 获取三列
+        y2 = [y[31] for y in im]  
+        y3 = [y[32] for y in im]  
+        ylist2=[y1,y2,y3]
+        VA.append(caculate_three_lines(ylist2))
+
+        y1 = [y[46] for y in im]  # 获取三列
+        y2 = [y[47] for y in im]  
+        y3 = [y[48] for y in im]  
+        ylist3=[y1,y2,y3]
+        VA.append(caculate_three_lines(ylist3))
         
-        # 水平半穿越
-        x = im[15]
-        x1 = x[0:32]
-        HA_H.append(caculate_line(x1))
-        x2 = x[32:64]
-        HA_H.append(caculate_line(x2))
+        '''水平半穿越'''
+        #上三行像素
+        x1 = im[14]
+        x2 = im[15]
+        x3 = im[16]
+        #左上
+        x_half_list1 =[x1[0:32],x2[0:32],x3[0:32]]
+        
+        HA_H.append(caculate_three_lines(x_half_list1))
+        #右上
+        x_half_list2 =[x1[32:64],x2[32:64],x3[32:64]]        
+        HA_H.append(caculate_three_lines(x_half_list2))
+        
+        #下三行像素
+        x1 = im[46]
+        x2 = im[47]
+        x3 = im[48]
+        #左下
+        x_half_list3 =[x1[0:32],x2[0:32],x3[0:32]]        
+        HA_H.append(caculate_three_lines(x_half_list3))
+        
+        #右下
+        x_half_list4 =[x1[32:64],x2[32:64],x3[32:64]]        
+        HA_H.append(caculate_three_lines(x_half_list4))
 
-        x = im[47]
-        x1 = x[0:32]
-        HA_H.append(caculate_line(x1))
-        x2 = x[32:64]
-        HA_H.append(caculate_line(x2))
+        '''竖直半穿越'''
+        #左三列
+        y1 = [y[14] for y in im]  # 获取列
+        y2 = [y[15] for y in im]
+        y3 = [y[16] for y in im]
+        #左上
+        y_half_list1 = [y1[0:32],y2[0:32],y3[0:32]]
+        VA_H.append(caculate_three_lines(y_half_list1))
+        #左下
+        y_half_list2 = [y1[32:64],y2[32:64],y3[32:64]]
+        VA_H.append(caculate_three_lines(y_half_list2))
 
-        # 竖直半穿越
-        y = [y[15] for y in im]  # 获取列
+        #右三列
+        y1 = [y[46] for y in im]  # 获取列
+        y2 = [y[47] for y in im]
+        y3 = [y[48] for y in im]
+        #右上
+        y_half_list3 = [y1[0:32],y2[0:32],y3[0:32]]
+        VA_H.append(caculate_three_lines(y_half_list3))
+        #右下
+        y_half_list4 = [y1[32:64],y2[32:64],y3[32:64]]
+        VA_H.append(caculate_three_lines(y_half_list4))
 
-        y1 = y[0:32]
-        VA_H.append(caculate_line(y1))
-        y2 = y[32:64]
-        VA_H.append(caculate_line(y2))
-
-        y = [y[47] for y in im]
-
-        y1 = y[0:32]
-        VA_H.append(caculate_line(y1))
-        y2 = y[32:64]
-        VA_H.append(caculate_line(y2))
-
+        
         # 测试打印特征值
         print pic_path
         print HA
@@ -66,7 +104,7 @@ def caculate_all(pathin):
         print HA_H
         print VA_H
         #
-               # 计算左上角的能量值
+        # 计算左上角的能量值
         cor_feature.append(cor_caculate(im, 0, 16, 0, 16))
         # 计算左下角的能量值
         cor_feature.append(cor_caculate(im, 48, 64, 0, 16))
@@ -96,6 +134,21 @@ def caculate_line(line):
             tag = 1
     return k
 
+'''
+三行像素，取最大的穿越值
+
+一：15,16,17
+二：31,32,33
+三：47,48,49
+'''
+def caculate_three_lines(lines):
+    #调用计算单行像素计算方法
+    c1=caculate_line(lines[0])
+    c2=caculate_line(lines[1])
+    c3=caculate_line(lines[2])
+
+    return max(c1,c2,c3)
+
 
 def cor_caculate(image, t_row, d_row, t_col, d_col):
     '''
@@ -118,7 +171,7 @@ def cor_caculate(image, t_row, d_row, t_col, d_col):
 
 def write_to_txt(ch_name, HA, VA, HA_H, VA_H, cor_feature):
     try:
-        fp = open("eng_value.txt", "a+")
+        fp = open("num_value.txt", "a+")
         fp.write(ch_name)
         fp.write(",")
 
@@ -149,4 +202,4 @@ def write_to_txt(ch_name, HA, VA, HA_H, VA_H, cor_feature):
 
 
 if __name__ == '__main__':
-    caculate_all('./res_english')
+    caculate_all('./res_number')
