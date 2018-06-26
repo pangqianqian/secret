80f6b13196ed770454fcf95b3da79548ebed90af
Q_T_Pang <wwwpqting@163.com>
2018-06-26 13:10:37 +0800

    修改密码

--- Database.py
+++ Database.py
@@ -8,7 +8,7 @@ class Database:
         print('连接到mysql服务器...')
         # 打开数据库连接
         # 用户名:hp, 密码:Hp12345.,用户名和密码需要改成你自己的mysql用户名和密码，并且要创建数据库TESTDB，并在TESTDB数据库中创建好表Student
-        db = mysql.connector.connect(user="root", passwd="ldh0517", database="Feature", charset="utf8")
+        db = mysql.connector.connect(user="root", passwd="****", database="Feature", charset="utf8")
         print '连接上了!'
         return db
 
